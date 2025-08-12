ac9edb48aa59f57e1f78870d3cb0ad619bffb418
serguc <42298249+Serguc@users.noreply.github.com>
2025-08-12 06:40:56 +0300

    Update device.mk

--- device.mk
+++ device.mk
@@ -40,7 +40,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl \
     android.hardware.health@2.1-service \
-    libhealthd.$(PRODUCT_PLATFORM)
     
 # AB
 AB_OTA_UPDATER := true
@@ -82,17 +81,6 @@ PRODUCT_PACKAGES += \
 # MTK PlPath Utils
 PRODUCT_PACKAGES += \
     mtk_plpath_utils.recovery
-
-# Additional Libraries
-TARGET_RECOVERY_DEVICE_MODULES += \
-    libkeymaster4 \
-    libkeymaster41 \
-    libpuresoftkeymasterdevice
-
-RECOVERY_LIBRARY_SOURCE_FILES += \
-    $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster4.so \
-    $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster41.so \
-    $(TARGET_OUT_SHARED_LIBRARIES)/libpuresoftkeymasterdevice.so
 
 # Vendor ramdisk
 PRODUCT_COPY_FILES += \
