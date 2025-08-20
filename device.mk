5be829d8c6e3b7ab1da652942d124adf4c915c86
serguc <42298249+Serguc@users.noreply.github.com>
2025-08-21 02:15:29 +0300

    Update device.mk

--- device.mk
+++ device.mk
@@ -86,7 +86,6 @@ PRODUCT_PACKAGES += \
 
 # Vendor ramdisk
 PRODUCT_COPY_FILES += \
-     device/Blackview/BV9200/fstab.emmc:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.emmc \
      device/Blackview/BV9200/fstab.mt6781:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.mt6781 
 
 # Otacert
