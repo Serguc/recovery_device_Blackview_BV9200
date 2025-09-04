995a6e4257e9ffa16fa41eb7ec4f254cdfb27dbb
serguc <42298249+Serguc@users.noreply.github.com>
2025-09-04 08:44:33 +0300

    Update device.mk

--- device.mk
+++ device.mk
@@ -28,8 +28,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_ven
 
 # Bootctrl
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.2-mtkimpl \
-    android.hardware.boot@1.2-service \
+   android.hardware.boot@1.2-mtkimpl \
     android.hardware.boot@1.2-mtkimpl.recovery
 
 PRODUCT_PACKAGES_DEBUG += \
@@ -42,9 +41,8 @@ PRODUCT_PACKAGES += \
 
 # Health HAL
 PRODUCT_PACKAGES += \
-    android.hardware.health@2.1-impl \
-    android.hardware.health@2.1-service \
-    android.hardware.health@2.1-impl.recovery
+    android.hardware.health@2.0-impl-2.1 \
+    android.hardware.health@2.1-service
     
 # AB
 AB_OTA_UPDATER := true
@@ -60,6 +58,7 @@ AB_OTA_PARTITIONS += \
     vbmeta_system \
     vbmeta_vendor \
     vendor \
+    product \
     
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
