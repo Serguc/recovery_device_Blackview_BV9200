77f7967600b1acbd2b485d3a943edabb0b98e6d2
serguc <42298249+Serguc@users.noreply.github.com>
2026-01-07 13:01:48 +0200

    Update twrp_BV9200.mk

--- twrp_BV9200.mk
+++ twrp_BV9200.mk
@@ -14,6 +14,9 @@
 # limitations under the License.
 #
 
+# Define hardware platform
+PRODUCT_RELEASE_NAME := BV9200
+
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
